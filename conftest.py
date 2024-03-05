207f8cc5272f3273a5812f7df9a89b56f0d4c161
elenayanushevskaya <qahelping@gmail.com>
2024-03-05 22:03:29 +0300

    -- create architecture

---
+++ conftest.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def driver():
+    web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    web_driver.implicitly_wait(5)
+    web_driver.minimize_window()
+
+    yield web_driver
+
+    web_driver.quit()
---
+++ data/__init__.py
@@ -0,0 +1,1 @@
+from data.urls import DOMEN
